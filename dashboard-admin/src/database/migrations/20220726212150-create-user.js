2b61677f8281c5c085e44107bb0421921e9111f9
JoiaribeAlves <joiaribegalves@protonmail.com>
2022-07-26 17:32:56 -0400

    Atualiza a Migration User

--- dashboard-admin/src/database/migrations/20220726212150-create-user.js
+++ dashboard-admin/src/database/migrations/20220726212150-create-user.js
@@ -1,33 +1,43 @@
-'use strict';
+"use strict";
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING
-      },
-      password_hash: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
-  }
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable("users", {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			name: {
+				type: Sequelize.STRING,
+			},
+			email: {
+				allowNull: false,
+				unique: true,
+				type: Sequelize.STRING,
+			},
+			password_hash: {
+				type: Sequelize.STRING,
+			},
+			role: {
+				allowNull: false,
+				type: Sequelize.ENUM("admin", "manager", "developer"),
+			},
+			status: {
+				allowNull: false,
+				type: Sequelize.ENUM("active", "archived"),
+			},
+			created_at: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updated_at: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable("users");
+	},
 };
